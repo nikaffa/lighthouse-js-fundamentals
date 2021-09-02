8bf6cc28bee8f49992bbf8195534c8dba7892b50
Veronika Fatikhova <fidelio@list.ru>
2021-09-02 22:44:40 +0000

    concantination arrays_updated

--- concat.js
+++ concat.js
@@ -1,7 +1,6 @@
 //concantinating two arrays
 const concat = (array1, array2) => {
-  let array = [];
-  return array = array1.concat(array2);
+  return array1.concat(array2);
 };
 
-console.log(concat([ 5, 10 ], []));
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]));
