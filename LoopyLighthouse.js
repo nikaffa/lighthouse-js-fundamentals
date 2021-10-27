50a3917b81603a4c7702caa4ee9fbe1de20645dc
Veronika Fatikhova <fidelio@list.ru>
2021-10-26 17:28:34 -0700

    LoolyLighthoude modified

--- LoopyLighthouse.js
+++ LoopyLighthouse.js
@@ -1,18 +1,13 @@
 //Loop over number from 100 to 200 and print them with the exceptions
 for (let number = 100; number <= 200; number ++) {
-  //if both multiple of 3 and  4
-  if (number % 3 === 0 && number % 4 === 0) {
-    console.log("LoopyLighthouse");
-  }
+  let output = "";
   //if a miltiple of 3
-  else if (number % 3 === 0) {
-    console.log("Loopy");
+  if (number % 3 === 0) {
+    output += "Loopy";
   }
   //if a multiple of 4
-  else if (number % 4 === 0) {
-    console.log("Lighthouse");
+  if (number % 4 === 0) {
+    output += "Lighthouse";
   }
-  else {
-    console.log(number);
-  } 
+  console.log(output === "" ? number : output);
 }
